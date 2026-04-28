2ca0e7833cd107018a44269652aef866a0d85303
Tushar_lalwani <lalwanit34@gmail.com>
2026-04-28 21:00:08 +0500

    Implements hero section

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,23 +5,26 @@ const NavBar = () => {
   return (
     <header>
       <nav>
+        {/* Left - Apple Logo */}
         <img src="/logo.svg" alt="Apple logo" />
 
+        {/* Center - Nav Links */}
         <ul>
           {navLinks.map(({ label }) => (
             <li key={label}>
-              <a href={label}>{label}</a>
+              <a href={`#${label}`}>{label}</a>
             </li>
           ))}
         </ul>
 
+        {/* Right - Icons */}
         <div className="flex-center gap-3">
-            <button>
-                <img src="/search.svg" alt="Search" />
-            </button>
-            <button>
-                <img src="/cart.svg" alt="Cart" />
-            </button>
+          <button>
+            <img src="/search.svg" alt="Search" />
+          </button>
+          <button>
+            <img src="/cart.svg" alt="Cart" />
+          </button>
         </div>
       </nav>
     </header>
